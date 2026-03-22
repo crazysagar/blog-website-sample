92c0e577ecbbd4c460e1031533ed6e1cb8237239
Sagar Shrestha <crazysagar9876543210@gmail.com>
2026-03-22 18:32:42 +0545

    edited create and update mode

--- admin/process.php
+++ admin/process.php
@@ -18,7 +18,37 @@ if (isset($_POST["create"])) {
 
     if ($result) {
     echo "Data inserted successfully";
-    header("location: http://localhost/CMS%20Project/admin/index.php");
+    header("location: http://http://localhost/CMS-Project/admin/index.php");
+
+} else {
+    die("Error: " . mysqli_error($conn));
+}
+}
+?>
+
+
+<?php
+
+if (isset($_POST["update"])) {
+
+        print_r($_POST);
+
+    include '../connect.php';
+
+    $title = mysqli_real_escape_string( $conn, $_POST["title"]);
+    $summary = mysqli_real_escape_string( $conn, $_POST["summary"]);
+    $content = mysqli_real_escape_string( $conn, $_POST["content"]);
+    $date = mysqli_real_escape_string( $conn, $_POST["date"]);
+    $id = mysqli_real_escape_string( $conn, $_POST["id"]);
+
+
+    $sqlInsert = "UPDATE posts SET  title = '$title', summary = '$summary', content = '$content', date = '$date' WHERE id = '$id'";
+
+    $result = mysqli_query($conn, $sqlInsert);
+
+    if ($result) {
+    echo "Data inserted successfully";
+    header("location: http://http://localhost/CMS-Project/admin/index.php");
 
 } else {
     die("Error: " . mysqli_error($conn));
