498003569a70c874c9e0d8e5bc4e46507eaf9103
Sagar Shrestha <crazysagar9876543210@gmail.com>
2026-03-23 14:01:30 +0545

    added delete feature

---
+++ admin/delete.php
@@ -0,0 +1,19 @@
+<?php
+$id = $_GET["id"];
+if($id) {
+    include("../connect.php");
+    $sqlDelete = "DELETE FROM posts WHERE id = $id ";
+    if(mysqli_query($conn, $sqlDelete)) {
+        header ("location:index.php");
+    } else {
+        die("Something is not right. Data is not deleted");
+    }
+
+} else {
+
+    echo"Post not found";
+}
+
+
+
+?>
